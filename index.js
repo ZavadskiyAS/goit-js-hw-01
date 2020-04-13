7ea0e495345e0756305afef38ce49669f769b5b3
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-13 17:49:45 +0300

    script 1-2

---
+++ index.js
@@ -0,0 +1,19 @@
+// скрипт 1
+const name = "Генератор защитного поля";
+let price = 1000;
+console.log('Выбран '+name+', цена за штуку '+price+' кредитов');
+price = 2000;
+console.log('Выбран '+name+', цена за штуку '+price+' кредитов');
+
+// скрипт 2
+const total = 100;
+const ordered = 50;
+let res;
+if (ordered > total) {
+    res = "На складе недостаточно твоаров!";
+} else {
+    res = "Заказ оформлен, с вами свяжется менеджер";
+}
+console.log(res);
+
+// скрипт 3
