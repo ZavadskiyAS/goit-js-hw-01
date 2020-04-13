d94419aecab372f8dd40078d629a2b7c0879d227
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-13 20:34:03 +0300

    script 3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 // скрипт 1
 const name = "Генератор защитного поля";
 let price = 1000;
@@ -17,3 +18,17 @@ if (ordered > total) {
 console.log(res);
 
 // скрипт 3
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+const enter = prompt('Your password');
+if (enter === null) {
+    message = 'Отменено пользователем!';
+} else if (enter === ADMIN_PASSWORD) {
+    message = "Добро пожаловать";
+} else  {//(enter !== ADMIN_PASSWORD); {
+    message = "Доступ запрещен, неверный пароль!";
+}
+alert(message);
+
+// скрипт 4
+
