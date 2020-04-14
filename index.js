2d3d0bd5e8dfc3d9dbf35bfc96b7e9ad82a5e4cd
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-14 19:43:00 +0300

    problem 6

--- index.js
+++ index.js
@@ -54,32 +54,62 @@
 //}  
 //alert(message);
 
-//const country = prompt("Введите страну");
-//let price;
-//let message;
 
-//switch (country.toLowerCase()) {
-//    case 'китай':
-//        price = 100;
-//        message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
+// скрипт 5
+// const country = prompt("Введите страну");
+// let price;
+// let message;
+// if (country === null) {
+//        console.log('Отменено пользователем!');
+// }
+// switch (country.toLowerCase()) {
+//     case 'китай':
+//         price = 100;
+//         message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
+//         break;
+//     case 'чили':
+//         price = 250;
+//         message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
+//         break;
+//     case 'австралия':
+//         price = 170;
+//         message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
+//         break;
+//     case 'индия':
+//         price = 80;
+//         message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
 //        break;
-//    case 'чили':
-//        price = 250;
-//        message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
-//        break;
-//    case 'австралия':
-//        price = 170;
-//        message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
-//        break;
-//    case 'индия':
-//        price = 80;
-//        message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
-//        break;
-//    case 'ямайка':
-//        price = 120;
-//        message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
-//        break;
-//    default:
-//        message = "В вашей стране доставка не доступна";
-//}
-//alert(message);
+//     case 'ямайка':
+//         price = 120;
+//         message = "Доставка в "+ country + " будет стоить " + price + " кредитов";
+//         break;
+//     default:
+//         message = "В вашей стране доставка не доступна";
+// }
+// alert(message);
+
+
+// скрипт 6
+
+let input;
+let total = 0;
+
+do {
+    input = prompt('Введите число');
+    if (input === null) {
+        break;
+    }
+
+    if (Number.isNaN(Number(input))) {
+        alert('Было введено не число, попробуйте еще раз');
+    } else {
+        total = total + Number(input);
+    }
+}
+while (input !== null);
+
+alert("Общая сумма чисел равна " + total +".");
+
+
+
+
